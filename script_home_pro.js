7ebdcaa68cf0c12a4088b1fe36428b42ce3754cf
luugiabaocreate-gif <luugiabao.create@gmail.com>
2025-10-25 22:55:57 +0700

    update frontend code

--- script_home_pro.js
+++ script_home_pro.js
@@ -1626,6 +1626,7 @@ btnPostStory.addEventListener("click", async () => {
 
   const formData = new FormData();
   formData.append("story", file);
+  formData.append("media", file);
 
   try {
     const res = await fetch(`${API_URL}/api/stories`, {
